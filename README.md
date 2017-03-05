4a96ecc9852cc40edaa1bf174c8bca8aea619e5a
Devinda Senanayaka <dsenanayaka1@gmail.com>
2017-03-05 00:15:45 -0600

    Removed api keys

--- README.md
+++ README.md
@@ -1,4 +1,24 @@
 # MST-Gym-Notifications
 
 Checks the MST Gym google sheet every 30 minuites to see if there is fewer than 35 people in the gym.
-Sends a push message onto this [channel](https://www.pushbullet.com/channel) if there is any update
+Sends a push message onto this [channel](https://www.pushbullet.com/channel) if there is any update.
+
+Can also be configured to read off any google spreadsheet and push to any channel
+
+Requires a config.json file with the following format
+{
+  "pushbulletApi": "API HERE",
+  "googleApi": "API HERE",
+  "cell": "B11",
+  "timezone": "America/Chicago",
+  "spreadsheet": "tA8NerswljZvsnzu5hIZHRg",
+  "maxPeople": 35,
+  "channelName": "mstgym",
+  "message": {
+    "title": "Get to the GYM"
+  },
+  "time": {
+    "max": 21,
+    "min": 6
+  }
+  }
--- gymNotifier.js
+++ gymNotifier.js
@@ -1,15 +1,19 @@
 var https = require('https');
 var request = require('request');
 var moment = require('moment-timezone');
+var config = require('./config.json');
 
+var googleApi = config.googleApi;
+var pushbulletApi = config.pushbulletApi;
+var cell = config.cell;
 var gymCount = 0;
-var time = moment().tz("America/Chicago");
+var time = moment().tz(config.timezone);
 
 var checkForPeople = function() {
   https.get({
     hostname: 'sheets.googleapis.com',
     port: 443,
-    path: '/v4/spreadsheets/tA8NerswljZvsnzu5hIZHRg/values/B11?key=AIzaSyC9SFuRFmL6uEvUiW3J7hVfG7qqAD0wFyk',
+    path: '/v4/spreadsheets/' + config.spreadsheet + '/values/' + cell + '?key=' + googleApi,
     agent: false
   }, (res) => {
     res.setEncoding('utf8');
@@ -18,28 +22,28 @@ var checkForPeople = function() {
     res.on('end', () => {
       let parsedData = JSON.parse(rawData);
       gymCount = parsedData['values'][0][0];
-      if (gymCount <= 35 && time > 6 && time < 21) {
+      // if (gymCount <= config.maxPeople && time > config.time.min && time < config.time.max) {
         request({
           method: 'POST',
           uri: 'https://api.pushbullet.com/v2/pushes',
           headers: {
-            'Access-Token': 'o.NgVnRz2GT98lrMIJyk6SdKlBV4WdVZyV',
+            'Access-Token': pushbulletApi,
           },
           body: {
-            "body": "GYM GYM GYm",
-            "title": "Get to the GYM",
+            "body": "There are " + gymCount + " people at the gym !",
+            "title": config.message.title,
             "type": "note",
-            "channel_tag": "mstgym"
+            "channel_tag": config.channelName
           },
           json: true
         }, function callBack(err, httpResponse, body) {
           console.log(body);
         })
-      }
+      // }
       console.log(parsedData['values'][0][0]);
-    })
-  })
+    });
+  });
 }
 
 checkForPeople();
-// setInterval(checkForPeople, 30 * 60 * 1000);
+setInterval(checkForPeople, 30 * 60 * 1000);
