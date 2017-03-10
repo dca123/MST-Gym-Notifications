b963dcc27d37a07b5fe096bc8a5206155235cd63
Devinda Senanayaka <dsenanayaka1@gmail.com>
2017-03-10 00:19:15 -0600

    Changed console outputs. Fixed issue to ensure that time is updated every loop

--- gymNotifier.js
+++ gymNotifier.js
@@ -7,42 +7,47 @@ var googleApi = config.googleApi;
 var pushbulletApi = config.pushbulletApi;
 var cell = config.cell;
 var gymCount = 0;
-var time = moment().tz(config.timezone).hour();
+var time;
 
 var checkForPeople = function() {
-  https.get({
-    hostname: 'sheets.googleapis.com',
-    port: 443,
-    path: '/v4/spreadsheets/' + config.spreadsheet + '/values/' + cell + '?key=' + googleApi,
-    agent: false
-  }, (res) => {
-    res.setEncoding('utf8');
-    let rawData = '';
-    res.on('data', (chunk) => rawData += chunk);
-    res.on('end', () => {
-      let parsedData = JSON.parse(rawData);
-      gymCount = parsedData['values'][0][0];
-      if (gymCount <= config.maxPeople && time > config.time.min && time < config.time.max) {
-        request({
-          method: 'POST',
-          uri: 'https://api.pushbullet.com/v2/pushes',
-          headers: {
-            'Access-Token': pushbulletApi,
-          },
-          body: {
-            "body": "There are " + gymCount + " people at the gym !",
-            "title": config.message.title,
-            "type": "note",
-            "channel_tag": config.channelName
-          },
-          json: true
-        }, function callBack(err, httpResponse, body) {
-          console.log(body);
-        })
-      }
-      console.log(parsedData['values'][0][0]);
+  time = moment().tz(config.timezone).hour()
+  if (time > config.time.min && time < config.time.max) {
+    https.get({
+      hostname: 'sheets.googleapis.com',
+      port: 443,
+      path: '/v4/spreadsheets/' + config.spreadsheet + '/values/' + cell + '?key=' + googleApi,
+      agent: false
+    }, (res) => {
+      res.setEncoding('utf8');
+      let rawData = '';
+      res.on('data', (chunk) => rawData += chunk);
+      res.on('end', () => {
+        let parsedData = JSON.parse(rawData);
+        gymCount = parsedData['values'][0][0];
+        if (gymCount <= config.maxPeople) {
+          request({
+            method: 'POST',
+            uri: 'https://api.pushbullet.com/v2/pushes',
+            headers: {
+              'Access-Token': pushbulletApi,
+            },
+            body: {
+              "body": "There are " + gymCount + " people at the gym !",
+              "title": config.message.title,
+              "type": "note",
+              "channel_tag": config.channelName
+            },
+            json: true
+          }, function callBack(err, httpResponse, body) {
+            console.log("pushed message");
+          })
+        }
+        console.log(parsedData['values'][0][0] " people in the gym");
+      });
     });
-  });
+  } else {
+    console.log("Gym Closed")
+  }
 }
 
 checkForPeople();
